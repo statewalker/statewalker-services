8d5aae650bc8e1eab9335cc0bc405fd783dfcc97
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-07-11 18:38:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { default as ns } from "@statewalker/ns";
 import * as services from "./src/index.js"
-Object.assign(ns, services);
+if (!ns.services) { Object.assign(ns, services); }
 export * from './src/index.js';
