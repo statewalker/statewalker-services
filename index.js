7032112711513539cda46d7fddf9a0f06259d875
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-07-12 11:41:24 +0200

    fix the export of services in the global namespace

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import { default as ns } from "@statewalker/ns";
-import * as services from "./src/index.js"
-if (!ns.services) { Object.assign(ns, services); }
-export * from './src/index.js';
+import * as lib from "./src/index.js"
+// Expose the "services" and other methods in the global namespace.
+// Methods are assigned to the namespace only if it are not defined yet.
+if (!ns.services) { Object.assign(ns, lib);  }
+export const {
+    newService,
+    newServices,
+    publishService,
+    resolveDependencies,
+    services
+} = ns;
