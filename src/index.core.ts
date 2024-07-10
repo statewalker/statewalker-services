16f2b8ffb037533af074e5063f41df858c63596d
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-07-10 20:24:42 +0200

    chore: update build configuration

--- src/index.core.ts
+++ src/index.core.ts
@@ -1,7 +1,4 @@
 export * from "./newService.ts";
 export * from "./newServices.ts";
-export * from "./resolver.ts";
-export * from "./resolveDependencies.ts";
-export * from "./publishService.ts"
 export * from "./services.ts";
 export * from "./types.ts";
